f07131ebf945bd108b454772c5595e731c0a1576
hajar-creator <145220121+hajar-creator@users.noreply.github.com>
2024-07-20 23:55:27 +0200

    simplifié fonction qui supprime les points existants

--- assets/script.js
+++ assets/script.js
@@ -1,74 +1,64 @@
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+    {
+        "image": "slide1.jpg",
+        "tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
+    },
+    {
+        "image": "slide2.jpg",
+        "tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+    },
+    {
+        "image": "slide3.jpg",
+        "tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
+    },
+    {
+        "image": "slide4.png",
+        "tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
+    }
+];
 
-
-
-let arrowLeft = document.querySelector(".arrow_left")
-let arrowRight = document.querySelector(".arrow_right")
+let arrowLeft = document.querySelector(".arrow_left");
+let arrowRight = document.querySelector(".arrow_right");
 
 arrowLeft.addEventListener("click", () => {
-    arrowChange("left")
-})
+    arrowChange("left");
+});
 arrowRight.addEventListener("click", () => {
-    arrowChange("right")
-})
+    arrowChange("right");
+});
 
-let index = 0
-bulletPoints()
-function bulletPoints (){
-    for (let i= 0; i<slides.length; i++){
-        let dots = document.querySelector(".dots")
-        let dotDiv = document.createElement("div")
-        dotDiv.classList.add("dot")
-		dotDiv.classList.add("dot-"+i)
-        dots.appendChild(dotDiv)
-        if (i === index){
-            dotDiv.classList.add("dot_selected")
+let index = 0;
+
+function bulletPoints() {
+    let dots = document.querySelector(".dots");
+    dots.innerHTML = ''; // Efface les points existants
+    for (let i = 0; i < slides.length; i++) {
+        let dotDiv = document.createElement("div");
+        dotDiv.classList.add("dot");
+        if (i === index) {
+            dotDiv.classList.add("dot_selected");
         }
+        dots.appendChild(dotDiv);
     }
+}
 
-}
-function bulletPointsUpdate(){
-	//au clique sur la flèche je rentre dans cette fonction et je dois 
-	//sur quel je me trouve et sur quel dot je dois aller
-	let dotSelected = document.querySelector(".dot_selected")
-	dotSelected.classList.remove("dot_selected")
-	let dotNext = document.querySelector(".dot-"+index)
-	dotNext.classList.add("dot_selected")
-}
 function updateSlide() {
-    let imageChange = document.querySelector(".banner-img") 
-    imageChange.setAttribute("src", `./assets/images/slideshow/${slides[index].image}`) 
+    let imageChange = document.querySelector(".banner-img");
+    imageChange.setAttribute("src", `./assets/images/slideshow/${slides[index].image}`);
 
-    let tagLineElement = document.querySelector("#banner p") 
-    tagLineElement.innerHTML = slides[index].tagLine 
+    let tagLineElement = document.querySelector("#banner p");
+    tagLineElement.innerHTML = slides[index].tagLine;
 
-    bulletPointsUpdate() 
+    bulletPoints();
 }
 
 function arrowChange(direction) {
     if (direction === "right") {
-        index = (index + 1) % slides.length 
+        index = (index + 1) % slides.length;
     } else if (direction === "left") {
-        index = (index - 1 + slides.length) % slides.length 
+        index = (index - 1 + slides.length) % slides.length;
     }
-    updateSlide() 
+    updateSlide();
 }
 
-
+updateSlide(); 
